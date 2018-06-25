aabac49d3856ff7e4c36cd37fb9febf982a60e0c
Yulia Tsyutsyura <yulia.tsyutsyura@devellar.com>
2018-06-25 14:36:52 +0300

    add new tests

--- src/test/java/taskLoginPage/Tests/TestSignUp.java
+++ src/test/java/taskLoginPage/Tests/TestSignUp.java
@@ -17,25 +17,5 @@ public class TestSignUp extends General{
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(signUp.getEmailLocator()));
          assertEquals("You a still on Login page", signUp.getURL(), driver.getCurrentUrl().toString());
     }
-    /*@Test
-    public void testCreateNewClient (){
-        LoginPage loginPage = new LoginPage(driver);
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(loginPage.getForgotLinkLocator()));
-
-       SignUp signUp = loginPage.beginSignUp();
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(signUp.getEmailLocator()));
-
-       *//* signUp.enterEmail("test");
-        signUp.enterPassword("test");
-        signUp.enterConfirmPassword("test");*//*
-        sleep(20);
-        signUp.agreeTerms();
-        wait.until(ExpectedConditions.elementToBeSelected(signUp.getAgreeTermsCheckboxLocator()));
-        signUp.agreeEmail();
-        wait.until(ExpectedConditions.elementToBeSelected(signUp.getAgreeEmailLocator()));
-        signUp.agreeCookies();
-        wait.until(ExpectedConditions.elementToBeSelected(signUp.getAgreeCookiesLocator()));
-
-    }*/
 }
